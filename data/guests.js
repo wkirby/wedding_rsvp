400b51dfbeda7baa3e188965603242ea9b553371
Wyatt Kirby <wyatt@apsis.io>
2019-05-20 09:57:15 -0700

    Add exported guest list

--- data/guests.js
+++ data/guests.js
@@ -1,14 +1,213 @@
-
 export const GUESTS = [
-    {
-      guestName: "Noah Callaway",
-      partnerName: "Ashley Lindsey"
-    },
-    {
-      guestName: "Ilsa Kirby"
-    },
-    {
-      guestName: "Tim Kretchmer",
-      plusOne: true
-    }
-  ];
+  {
+    guestName: "Jennifer Swatsky",
+    partnerName: "Tom Mullen"
+  },
+  {
+    guestName: "Richard Bourget",
+    partnerName: "Robin Shannon"
+  },
+  {
+    guestName: "Jarrod Bourget",
+    partnerName: "Amanda Hersom"
+  },
+  {
+    guestName: "Mémé Bourget"
+  },
+  {
+    guestName: "Doris Hould"
+  },
+  {
+    guestName: "Denise Thibault",
+    partnerName: "Steve Edwin"
+  },
+  {
+    guestName: "Paul Bourget"
+  },
+  {
+    guestName: "Richard Thibault",
+    partnerName: "Adam Cannata"
+  },
+  {
+    guestName: "Jamie Thibault",
+    partnerName: "Dane Krauth"
+  },
+  {
+    guestName: "Joanie William"
+  },
+  {
+    guestName: "Nellie Towers",
+    partnerName: "Bil Towers"
+  },
+  {
+    guestName: "Sandy Swatsky",
+    partnerName: "Tom Bassford"
+  },
+  {
+    guestName: "Cliff Ostrowski"
+  },
+  {
+    guestName: "Adam Collemer",
+    partnerName: "Sara Fricke"
+  },
+  {
+    guestName: "Amy Sweet",
+    plusOne: true
+  },
+  {
+    guestName: "Julia Bonaventure",
+    partnerName: "Chris Stockdell"
+  },
+  {
+    guestName: "Eliza Tilbor",
+    plusOne: true
+  },
+  {
+    guestName: "Claudia Jacob"
+  },
+  {
+    guestName: "Casey Middaugh"
+  },
+  {
+    guestName: "Lauren Bond",
+    partnerName: "Steph the Crow"
+  },
+  {
+    guestName: "Mischelle Schmidt",
+    partnerName: "Matt Schmidt"
+  },
+  {
+    guestName: "John Ostrowski"
+  },
+  {
+    guestName: "Becky Ostrowski"
+  },
+  {
+    guestName: "Mat Schmidt",
+    partnerName: "Sara Hall"
+  },
+  {
+    guestName: "Cole Whitney"
+  },
+  {
+    guestName: "Karen Tilbor"
+  },
+  {
+    guestName: "Darlene Saeger",
+    partnerName: "Pat Saeger"
+  },
+  {
+    guestName: "Jackie Towers",
+    plusOne: true
+  },
+  {
+    guestName: "Pam Sadlon",
+    partnerName: "Andy Sadlon"
+  },
+  {
+    guestName: "Andrea Scott",
+    partnerName: "Jeff Scott"
+  },
+  {
+    guestName: "Ken Kirby"
+  },
+  {
+    guestName: "Natasha Okonogi",
+    partnerName: "Hal Neth"
+  },
+  {
+    guestName: "Dr. Ilsa Kirby"
+  },
+  {
+    guestName: "Raina Okonogi-Neth"
+  },
+  {
+    guestName: "Blaze Okonogi-Neth"
+  },
+  {
+    guestName: "Lyle Janousek"
+  },
+  {
+    guestName: "Rae Swent"
+  },
+  {
+    guestName: "Liz Stevens",
+    partnerName: "Mike Stevens"
+  },
+  {
+    guestName: "Rachel Stevens"
+  },
+  {
+    guestName: "Jacob Stevens"
+  },
+  {
+    guestName: "Nancy Stevens"
+  },
+  {
+    guestName: "Doug Kirby",
+    partnerName: "Britta Kirby"
+  },
+  {
+    guestName: "Steve Kirby",
+    partnerName: "Karen Titel"
+  },
+  {
+    guestName: "Mark Kirby",
+    partnerName: "Lisa Kirby"
+  },
+  {
+    guestName: "Ava Kirby"
+  },
+  {
+    guestName: "Karen Tennant",
+    partnerName: "Brad Tennant"
+  },
+  {
+    guestName: "Shirley Kirby"
+  },
+  {
+    guestName: "Tammy Jovonovich",
+    partnerName: "Steve Jovonovich"
+  },
+  {
+    guestName: "Noah Callaway",
+    partnerName: "Ashley Lindsey"
+  },
+  {
+    guestName: "Dr. Karen Keiter",
+    partnerName: "Henry Keiter"
+  },
+  {
+    guestName: "Travis Kirby"
+  },
+  {
+    guestName: "Faye Wylder",
+    partnerName: "Adam Hirsch"
+  },
+  {
+    guestName: "Eric Greer",
+    partnerName: "Heather Greer"
+  },
+  {
+    guestName: "Chris Pfohl",
+    partnerName: "Britt Pfohl"
+  },
+  {
+    guestName: "Tim Kretchmer"
+  },
+  {
+    guestName: "Nic Villasenor",
+    partnerName: "Jeremy Villasenor"
+  },
+  {
+    guestName: "Niall Wingham",
+    partnerName: "Nyla Wingham"
+  },
+  {
+    guestName: "Mo Cohen"
+  },
+  {
+    guestName: "Richard Donaldson",
+    partnerName: "Jetty Donaldson"
+  }
+];
